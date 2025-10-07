eb43b45d6305cfdd8a0584ce083f87d42bbd6e2c
silvia.garciabouza <silvia.gar.bou.dev@gmail.com>
2025-10-07 18:20:46 +0200

    a

--- src/main/java/teoria/ej19_json/alumno/AppAlumnos.java
+++ src/main/java/teoria/ej19_json/alumno/AppAlumnos.java
@@ -6,64 +6,98 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 
 public class AppAlumnos {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// Parte1: Lee el fichero de alumnos, crear una lista de alumnos y mostrarlos
-		// por consola
-		//TODO Hacer por parte del alumno
+        // Parte1: Lee el fichero de alumnos, crear una lista de alumnos y mostrarlos
+        // por consola
+        //TODO Hacer por parte del alumno
+//        for(Alumno e: leerJson(new File("src/main/java/teoria/ej19_json/alumno/alumnos_asignatura.json"))){
+//            System.out.println(e.getNombre());
+//        }
+        leerJson(new File("src/main/java/teoria/ej19_json/alumno/alumnos_asignatura.json")).forEach(System.out::println);
+        // Parte 2: Generar y escribir en el archivo alumnos_output.json
+        //TODO Hacer por parte del alumno
 
-		// Parte 2: Generar y escribir en el archivo alumnos_output.json
-		//TODO Hacer por parte del alumno
+        // Parte 3: Generar y escribir en el archivo alumnos_estadisticas.json
+        //TODO Hacer por parte del alumno
 
-		// Parte 3: Generar y escribir en el archivo alumnos_estadisticas.json
-		//TODO Hacer por parte del alumno
-		
-		System.out.println("Done!");
-	}
-	
-	
-	
-	private static void escribirJson(File fichero) {
-		throw new UnsupportedOperationException("A implementar por el alumno");
-	}
+        System.out.println("Done!");
+    }
 
-	private static List<Alumno> leerJson(File fichero) {
-		throw new UnsupportedOperationException("A implementar por el alumno");
-	}
 
-	private static void escribirEstadisticasAlumnos(List<Alumno> alumnos) {
-		throw new UnsupportedOperationException("A implementar por el alumno");
-	}
+    private static void escribirJson(File fichero) {
 
-	private static List<Asignatura> calcularMediaAsignaturas(List<Alumno> alumnos) {
-		throw new UnsupportedOperationException("A implementar por el alumno");
-	}
+    }
 
-	private static List<Alumno> crearAlumnosFicticios() {
-		List<Alumno> alumnos = new ArrayList<>();
+    private static List<Alumno> leerJson(File fichero) {
+        List<Alumno> listAlumnos = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fichero)))) {
 
-		// Primer alumno
-		List<Asignatura> asignaturas1 = new ArrayList<>();
-		asignaturas1.add(new Asignatura("BBDD", 9));
-		asignaturas1.add(new Asignatura("LM", 8));
-		Alumno alumno1 = new Alumno("12345678-T", "Alicia", "Castro", "Lopez", asignaturas1);
-		alumnos.add(alumno1);
 
-		// Segundo alumno
-		List<Asignatura> asignaturas2 = new ArrayList<>();
-		asignaturas2.add(new Asignatura("Programación", 10));
-		asignaturas2.add(new Asignatura("Redes", 7));
-		Alumno alumno2 = new Alumno("22345678-T", "Borja", "Sanchez", "Lopez", asignaturas2);
-		alumnos.add(alumno2);
+        JsonParser parser = new JsonParser();
+        JsonObject jsonob= parser.parse(br).getAsJsonObject();
 
-		return alumnos;
-	}
+            JsonArray gsonArr = jsonob.get("alumnos").getAsJsonArray();
+            for (JsonElement element : gsonArr) {
+                JsonObject gsonObj = element.getAsJsonObject();
+                String nombre = gsonObj.get("nombre").getAsString();
+                String dni = gsonObj.get("dni").getAsString();
+                String apellido1 = gsonObj.get("apellido1").getAsString();
+                String apellido2 = gsonObj.get("apellido2").getAsString();
+
+                JsonArray asignaturas = gsonObj.get("asignaturas").getAsJsonArray();
+                List listaAsignautas = new ArrayList<>();
+                for (JsonElement e : asignaturas) {
+                    JsonObject gsonObjAsig = e.getAsJsonObject();
+                    String name = gsonObjAsig.get("nombre").getAsString();
+                    double calificacion = gsonObjAsig.get("calificacion").getAsDouble();
+                    listaAsignautas.add(new Asignatura(name, calificacion));
+                }
+                Alumno a=new Alumno(nombre, dni, apellido1, apellido2, listaAsignautas);
+
+                listAlumnos.add(a);
+            }
+
+    } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+    }finally {
+            return listAlumnos;
+        }
+    }
+
+    private static void escribirEstadisticasAlumnos(List<Alumno> alumnos) {
+        throw new UnsupportedOperationException("A implementar por el alumno");
+    }
+
+    private static List<Asignatura> calcularMediaAsignaturas(List<Alumno> alumnos) {
+        throw new UnsupportedOperationException("A implementar por el alumno");
+    }
+
+    private static List<Alumno> crearAlumnosFicticios() {
+        List<Alumno> alumnos = new ArrayList<>();
+
+        // Primer alumno
+        List<Asignatura> asignaturas1 = new ArrayList<>();
+        asignaturas1.add(new Asignatura("BBDD", 9));
+        asignaturas1.add(new Asignatura("LM", 8));
+        Alumno alumno1 = new Alumno("12345678-T", "Alicia", "Castro", "Lopez", asignaturas1);
+        alumnos.add(alumno1);
+
+        // Segundo alumno
+        List<Asignatura> asignaturas2 = new ArrayList<>();
+        asignaturas2.add(new Asignatura("Programación", 10));
+        asignaturas2.add(new Asignatura("Redes", 7));
+        Alumno alumno2 = new Alumno("22345678-T", "Borja", "Sanchez", "Lopez", asignaturas2);
+        alumnos.add(alumno2);
+
+        return alumnos;
+    }
 
 }
